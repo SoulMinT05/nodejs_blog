365c03c10f1dbc8a5ab2d70b442a2903507c7154
tamsoul <tamsoul>
2024-06-03 16:16:17 +0700

    Static file and SCSS

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
 // HTTP logger
 app.use(morgan('combined'));
 
@@ -24,6 +25,7 @@ app.get('/', (req, res) => {
 app.get('/news', (req, res) => {
     res.render('news');
 });
+console.log('PATH: ', path.join(__dirname, 'public'));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
