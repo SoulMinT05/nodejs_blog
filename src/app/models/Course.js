dc112c99d708e36a43a96292ad61f079b6eeb332
tamsoul <tamsoul>
2024-07-24 15:33:41 +0700

    create new course

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,14 +1,22 @@
 const mongoose = require('mongoose');
+// const slug = require('mongoose-slug-generator');
+const slug = require('mongoose-slug-updater');
+mongoose.plugin(slug);
 
 const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
 
-const Course = new Schema({
-    name: { type: String, minLength: 1, maxLength: 255 },
-    description: { type: String, maxLength: 650 },
-    image: { type: String, maxLength: 255 },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
-});
+const Course = new Schema(
+    {
+        name: { type: String, required: true },
+        description: { type: String },
+        image: { type: String },
+        videoId: { type: String, required: true },
+        level: { type: String },
+        slug: { type: String, slug: 'name', unique: true },
+    },
+    {
+        timestamps: true,
+    },
+);
 
 module.exports = mongoose.model('Course', Course);
