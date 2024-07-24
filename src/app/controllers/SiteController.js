664ac7e90f61e1c440ab89dd9482ac5cf80f8d17
tamsoul <tamsoul>
2024-07-24 11:44:05 +0700

    render courses UI

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,14 +1,27 @@
 const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../utils/mongoose');
 
 class SiteController {
     // GET / home
-    async index(req, res) {
-        try {
-            const courses = await Course.find({});
-            res.json(courses);
-        } catch (err) {
-            console.log('err: ', err);
-        }
+    async index(req, res, next) {
+        // try {
+        //     const courses = await Course.find({}).lean();
+        //     res.render('home', { courses });
+        // } catch (err) {
+        //     next(err);
+        //     console.log('err: ', err);
+        // }
+
+        Course.find({})
+            .lean()
+            .then((courses) => {
+                // courses = courses.map((course) => course.toObject());
+                res.render('home', {
+                    courses,
+                    // courses: multipleMongooseToObject(courses),
+                });
+            })
+            .catch((err) => next(err));
     }
 
     // GET / search
