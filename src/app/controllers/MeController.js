c4f429a3e241f2a6f73be342ed1909bd39ffe0e1
tamsoul <tamsoul>
2024-07-25 21:20:26 +0700

    count deleted course

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -3,15 +3,40 @@ const { mongooseToObject } = require('../../utils/mongoose');
 
 class MeController {
     // GET /me/stored/courses
-    storedCourses(req, res, next) {
-        Course.find({})
-            .lean()
-            .then(courses => {
+    async storedCourses(req, res, next) {
+        //Without Promise, two Course execute async approximately at the same time,
+        //Course 1 has not output, Course 2 ran
+
+        //Example: if promise 1 run 2s, promise 2 run 5s
+        // If use 2 promise: promise 1 and promise 2 --> run total time: 7s
+        // But if use Promise.all --> run total time: 5s
+        Promise.all([
+            Course.find({}).lean(),
+            Course.countDocumentsWithDeleted({ deleted: true }),
+        ])
+            .then(([courses, deletedCount]) => {
                 res.render('me/stored-courses', {
                     courses,
+                    deletedCount,
                 });
             })
-            .catch(err => console.log('err: ', err));
+            .catch(next);
+
+        // Course.countDocumentsWithDeleted({ deleted: true })
+        //     .lean()
+        //     .then(deleteCount =>
+        //         console.log('deleteCount111111: ', deleteCount),
+        //     )
+        //     .catch(next);
+
+        // Course.find({})
+        //     .lean()
+        //     .then(courses => {
+        //         res.render('me/stored-courses', {
+        //             courses,
+        //         });
+        //     })
+        //     .catch(err => co nsole.log('err: ', err));
     }
     // GET /me/trash/courses
     trashCourses(req, res, next) {
